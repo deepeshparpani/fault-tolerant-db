97f33c9dcd46af6013cd1d5333d179e5df8e5cd5
Deepesh Parpani <deepesh.parpani@gmail.com>
2025-12-05 22:34:12 -0500

    4.1 GigaPaxos Implementation

--- src/server/faulttolerance/MyDBReplicableAppGP.java
+++ src/server/faulttolerance/MyDBReplicableAppGP.java
@@ -1,150 +1,114 @@
 package server.faulttolerance;
 
-import com.datastax.driver.core.Cluster;
-import edu.umass.cs.gigapaxos.interfaces.Replicable;
-import edu.umass.cs.gigapaxos.interfaces.Request;
-import edu.umass.cs.nio.interfaces.IntegerPacketType;
-import edu.umass.cs.nio.interfaces.NodeConfig;
-import edu.umass.cs.nio.nioutils.NIOHeader;
-import edu.umass.cs.nio.nioutils.NodeConfigUtils;
-import edu.umass.cs.reconfiguration.reconfigurationutils.RequestParseException;
+import com.datastax.driver.core.*;
+import edu.umass.cs.gigapaxos.interfaces.*;
+import edu.umass.cs.gigapaxos.paxospackets.*;
+import edu.umass.cs.nio.interfaces.*;
+import edu.umass.cs.reconfiguration.reconfigurationutils.*;
+import org.json.*;
+import java.io.*;
+import java.util.*;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.util.HashSet;
-import java.util.Set;
-
-/**
- * This class should implement your {@link Replicable} database app if you wish
- * to use Gigapaxos.
- * <p>
- * Make sure that both a single instance of Cassandra is running at the default
- * port on localhost before testing.
- * <p>
- * Tips:
- * <p>
- * 1) No server-server communication is permitted or necessary as you are using
- * gigapaxos for all that.
- * <p>
- * 2) A {@link Replicable} must be agnostic to "myID" as it is a standalone
- * replication-agnostic application that via its {@link Replicable} interface is
- * being replicated by gigapaxos. However, in this assignment, we need myID as
- * each replica uses a different keyspace (because we are pretending different
- * replicas are like different keyspaces), so we use myID only for initiating
- * the connection to the backend data store.
- * <p>
- * 3) This class is never instantiated via a main method. You can have a main
- * method for your own testing purposes but it won't be invoked by any of
- * Grader's tests.
- */
 public class MyDBReplicableAppGP implements Replicable {
 
-	/**
-	 * Set this value to as small a value with which you can get tests to still
-	 * pass. The lower it is, the faster your implementation is. Grader* will
-	 * use this value provided it is no greater than its MAX_SLEEP limit.
-	 * Faster
-	 * is not necessarily better, so don't sweat speed. Focus on safety.
-	 */
-	public static final int SLEEP = 1000;
+    public static final int SLEEP = 1000;
+    private Cluster cluster;
+    private Session session;
+    private String keyspace;
+    private static final String TABLE = "grade";
 
-	/**
-	 * All Gigapaxos apps must either support a no-args constructor or a
-	 * constructor taking a String[] as the only argument. Gigapaxos relies on
-	 * adherence to this policy in order to be able to reflectively construct
-	 * customer application instances.
-	 *
-	 * @param args Singleton array whose args[0] specifies the keyspace in the
-	 *             backend data store to which this server must connect.
-	 *             Optional args[1] and args[2]
-	 * @throws IOException
-	 */
-	public MyDBReplicableAppGP(String[] args) throws IOException {
-		// TODO: setup connection to the data store and keyspace
-		throw new RuntimeException("Not yet implemented");
-	}
+    public MyDBReplicableAppGP(String[] args) throws IOException {
+        if (args == null || args.length == 0) throw new IllegalArgumentException();
+        keyspace = args[0];
+        String host = "localhost";
+        if (args.length > 1 && args[1] != null) host = args[1];
+        cluster = Cluster.builder().addContactPoint(host).build();
+        session = cluster.connect(keyspace);
+    }
 
-	/**
-	 * Refer documentation of {@link Replicable#execute(Request, boolean)} to
-	 * understand what the boolean flag means.
-	 * <p>
-	 * You can assume that all requests will be of type {@link
-	 * edu.umass.cs.gigapaxos.paxospackets.RequestPacket}.
-	 *
-	 * @param request
-	 * @param b
-	 * @return
-	 */
-	@Override
-	public boolean execute(Request request, boolean b) {
-		// TODO: submit request to data store
-		throw new RuntimeException("Not yet implemented");
-	}
+    public boolean execute(Request request, boolean doNotReplyToClient) {
+        try {
+            String command = extractCommand(request);
+            if (command == null || command.trim().isEmpty()) return false;
+            
+            session.execute(command);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
-	/**
-	 * Refer documentation of
-	 * {@link edu.umass.cs.gigapaxos.interfaces.Application#execute(Request)}
-	 *
-	 * @param request
-	 * @return
-	 */
-	@Override
-	public boolean execute(Request request) {
-		// TODO: execute the request by sending it to the data store
-		throw new RuntimeException("Not yet implemented");
-	}
+    public boolean execute(Request request) {
+        return execute(request, false);
+    }
 
-	/**
-	 * Refer documentation of {@link Replicable#checkpoint(String)}.
-	 *
-	 * @param s
-	 * @return
-	 */
-	@Override
-	public String checkpoint(String s) {
-		// TODO:
-		throw new RuntimeException("Not yet implemented");
-	}
-
-	/**
-	 * Refer documentation of {@link Replicable#restore(String, String)}
-	 *
-	 * @param s
-	 * @param s1
-	 * @return
-	 */
-	@Override
-	public boolean restore(String s, String s1) {
-		// TODO:
-		throw new RuntimeException("Not yet implemented");
-
-	}
+    /**
+     * Helper to get the string command (CQL) from the generic Request object.
+     */
+    private String extractCommand(Request request) {
+        if (request instanceof RequestPacket) return ((RequestPacket) request).requestValue;
+        return request != null ? request.toString() : null;
+    }
 
 
-	/**
-	 * No request types other than {@link edu.umass.cs.gigapaxos.paxospackets
-	 * .RequestPacket will be used by Grader, so you don't need to implement
-	 * this method.}
-	 *
-	 * @param s
-	 * @return
-	 * @throws RequestParseException
-	 */
-	@Override
-	public Request getRequest(String s) throws RequestParseException {
-		return null;
-	}
 
-	/**
-	 * @return Return all integer packet types used by this application. For an
-	 * example of how to define your own IntegerPacketType enum, refer {@link
-	 * edu.umass.cs.reconfiguration.examples.AppRequest}. This method does not
-	 * need to be implemented because the assignment Grader will only use
-	 * {@link
-	 * edu.umass.cs.gigapaxos.paxospackets.RequestPacket} packets.
-	 */
-	@Override
-	public Set<IntegerPacketType> getRequestTypes() {
-		return new HashSet<IntegerPacketType>();
-	}
+    public String checkpoint(String name) {
+        try {
+            ResultSet resultSet = session.execute("SELECT * FROM " + keyspace + "." + TABLE + ";");
+            JSONObject json = new JSONObject();
+            JSONArray rowsArray = new JSONArray();
+            
+            for (Row row : resultSet) {
+                JSONObject obj = new JSONObject();
+                obj.put("id", row.getInt("id"));
+                List<Integer> list = row.getList("events", Integer.class);
+                JSONArray events = new JSONArray();
+                if (list != null)
+                    for (Integer i : list) events.put(i);
+                obj.put("events", events);
+                rowsArray.put(obj);
+            }
+            json.put("name", name);
+            json.put("rows", rowsArray);
+            return json.toString();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public boolean restore(String name, String state) {
+        try {
+            if (state == null || state.trim().isEmpty()) return true;
+            JSONObject json = new JSONObject(state);
+            JSONArray rowsArray = json.optJSONArray("rows");
+            if (rowsArray == null || rowsArray.length() == 0) return true;
+            
+            // Clear stale state
+            session.execute("TRUNCATE " + keyspace + "." + TABLE + ";");
+            
+            // Re-insert state from checkpoint
+            for (int i = 0; i < rowsArray.length(); i++) {
+                JSONObject obj = rowsArray.getJSONObject(i);
+                StringBuilder eventsStr = new StringBuilder("[");
+                JSONArray events = obj.getJSONArray("events");
+                for (int m = 0; m < events.length(); m++) {
+                    if (m > 0) eventsStr.append(",");
+                    eventsStr.append(events.getInt(m));
+                }
+                eventsStr.append("]");
+                session.execute("INSERT INTO " + keyspace + "." + TABLE + " (id, events) VALUES (" + obj.getInt("id") + ", " + eventsStr + ");");
+            }
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Request getRequest(String s) throws RequestParseException {
+        return null;
+    }
+
+    public Set<IntegerPacketType> getRequestTypes() {
+        return new HashSet<IntegerPacketType>();
+    }
 }
